2df58db54e3b0f23de793ebb795fa00b5cda33ac
SShGitHubUser <126820296+SShGitHubUser@users.noreply.github.com>
2023-03-08 13:50:00 +0200

    Delete const.py

--- const.py
+++
@@ -1,3 +0,0 @@
-OWM_URL = ''.join(['https://api.openweathermap.org/data/2.5/weather?q={city}&appid=', OWM_TOKEN,'&units=metric'])
-TGRAM_URL = ''.join(['https://api.telegram.org/bot', TGRAM_TOKEN, '/{method}'])
-LAST_UPDATE_ID_FILE = 'last_update_id.txt'
