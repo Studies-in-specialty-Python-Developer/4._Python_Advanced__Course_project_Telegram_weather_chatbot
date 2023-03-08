cb7d075016930b173cea4d64151ed8810f9bfb68
COMMON-PC\Common <shypulin@ukr.net>
2023-03-08 19:17:33 +0200

    08.03.2023 state

--- const_py_example.py
+++ const_py_example.py
@@ -1,10 +1,20 @@
+# Заменить на свои идентификаторы и переименовать файл в const.py
+
+ADMIN_ID = 'My_Telegram_ID'
+
 OWM_TOKEN = 'My_OpenWeatherMap_token'
+
 TGRAM_TOKEN = 'My_Telegram_token'
-OWM_URL = ''.join(['https://api.openweathermap.org/data/2.5/weather?q={city}&appid=', OWM_TOKEN, '&units=metric'])
+
+OWM_URL = ''.join(['https://api.openweathermap.org/data/2.5/weather?q={city}&appid=',
+                   OWM_TOKEN, '&units=metric&lang=ua'])
+
 TGRAM_URL = ''.join(['https://api.telegram.org/bot', TGRAM_TOKEN, '/{method}'])
+
 LAST_UPDATE_ID_FILE = 'last_update_id.txt'
+
 COMMANDS = {
     '/start': 'Здравствуйте!\nВведите название города',
     '/stop': 'До свидания!',
-    '/stopbot': 'Бот остановлен. Обработка сообщений прекращена.'
+    '/turnoff': 'Бот остановлен. Обработка сообщений прекращена.'
 }
--- main.py
+++ main.py
@@ -1,87 +1,74 @@
 import json
-import requests
 import time
 import os
-from pprint import pprint
-from const import *
+import requests
+# from pprint import pprint
+from const import ADMIN_ID, OWM_URL, TGRAM_URL, LAST_UPDATE_ID_FILE, COMMANDS
 
 
 def main():
     # Считываем из файла ID последнего обработанного сообщения
     last_update_id = 0
     if os.path.exists(LAST_UPDATE_ID_FILE):
-        with open(LAST_UPDATE_ID_FILE) as file:
+        with open(LAST_UPDATE_ID_FILE, encoding='utf-8') as file:
             data = file.readline()
             if data:
                 last_update_id = int(data)
 
-    # Получаем на сервере Telegram все сообщения, оставленные за последние 24 часа
-    url = TGRAM_URL.format(method='getUpdates')
-    response = requests.get(url)
-    json_data = json.loads(response.text)
+    turn_on = True
+    while turn_on:
+        # Получаем на сервере Telegram все сообщения, оставленные за последние 24 часа
+        url = TGRAM_URL.format(method='getUpdates')
+        response = requests.get(url, timeout=None)
+        mess_queue = json.loads(response.text)
 
-    pprint(json_data)
+        # Просматриваем сообщения в хронологическом порядке и обрабатываем те, у которых Update_ID больше,
+        # чем ID последнего обработанного сообщения
+        response_data = {}
+        for mess in mess_queue.get('result'):
+            if last_update_id < mess.get('update_id'):
 
-    # Просматриваем сообщения в хронологическом порядке и обрабатываем те, у которых Update_ID больше,
-    # чем ID последнего обработанного сообщения
-    message_data = dict()
-    for item in json_data.get('result'):
-        if last_update_id < item.get('update_id'):
+                # Получаем необходимые параметры сообщения
+                chat_id = mess.get('message').get('chat').get('id')
+                text = mess.get('message').get('text')
+                last_update_id = mess.get('update_id')
+                user_id = mess.get('message').get('from').get('id')
 
-            # Получаем необходимые параметры сообщения
-            chat_id = item.get('message').get('chat').get('id')
-            text = item.get('message').get('text')
-            last_update_id = item.get('update_id')
-
-            # Записываем в файл Update_ID текущего сообщения
-            with open(LAST_UPDATE_ID_FILE, 'w') as file:
-                file.write(str(last_update_id))
-
-            print(chat_id, text, last_update_id)
-
-            # Формируем ответ на сообщение
-            message_data['chat_id'] = chat_id
-            if COMMANDS.get(text):
-                message_data['text'] = COMMANDS[text]
-            else:
-                url = OWM_URL.format(city=text)
-                response = requests.get(url)
-                json_data = json.loads(response.text)
-
-                pprint(json_data)
-
-                if json_data.get('cod') != 200:
-                    message_data['text'] = f'Ошибка: {json_data.get("message")}'
+                # Формируем ответ на сообщение
+                response_data['chat_id'] = chat_id
+                if COMMANDS.get(text):
+                    response_data['text'] = COMMANDS[text]
+                    if text == '/turnoff':
+                        if str(user_id) == ADMIN_ID:
+                            turn_on = False
+                        else:
+                            response_data['text'] = 'Извините, у Вас нет прав администратора'
                 else:
-                    message_data['text'] = json_data
+                    url = OWM_URL.format(city=text)
+                    response = requests.get(url, timeout=None)
+                    if response.status_code in [200, 404]:
+                        json_data = json.loads(response.text)
+                        if json_data.get('cod') == 200:
+                            response_data['text'] = f'{json_data.get("name")}, {json_data.get("sys").get("country")}' \
+                                                    + f'\nТемпература: {json_data.get("main").get("temp")} С, ' \
+                                                    + f'давление: {json_data.get("main").get("pressure")} мбар, ' \
+                                                    + f'облачность: {list(json_data.get("clouds").values())[0]} %'
+                        else:
+                            response_data['text'] = f'Ошибка {json_data.get("cod")}: {json_data.get("message")}\n' \
+                                                    + 'Введите название города'
+                    else:
+                        response_data['text'] = f'Ошибка сети, код: {response.status_code}\n' \
+                                                + 'Попробуйте сделать запрос позже'
 
-            # Отвечаем на сообщение
-            url = TGRAM_URL.format(method='sendMessage')
-            response = requests.get(url, message_data)
-            json_data = json.loads(response.text)
+                # Отвечаем на сообщение
+                url = TGRAM_URL.format(method='sendMessage')
+                response = requests.get(url, response_data, timeout=None)
 
-            pprint(json_data)
+                # Записываем в файл Update_ID текущего сообщения
+                with open(LAST_UPDATE_ID_FILE, 'w', encoding='utf-8') as file:
+                    file.write(str(last_update_id))
 
-    time.sleep(3)
-
-    #
-    #     def get_weather(city):
-    #         city='Kharkiv'
-    #         url_weather = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={owm_token}'
-    #         response = requests.get(url_weather)
-    #         if response.status_code == 200:
-    #             json_data = json.loads(response.text)
-    #         else:
-    #             json_data = f'Ошибка при получении данных: код {response.status_code}'
-    #         return json_data
-    #
-
-    # dict_weather = dict()
-    # dict_weather['link'] = json_data[0]['MobileLink']
-    # dict_weather['сейчас'] = {'temp': json_data[0]['Temperature']['Value'], 'sky': json_data[0]['IconPhrase']}
-    # for i in range(len(json_data): 1:):
-    # time = 'через' + str(i) + 'ч'
-    # dict_weather[time] = {'temp': json_data[i]['Temperature']['Value'], 'sky': json_data[i]['IconPhrase']}
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
