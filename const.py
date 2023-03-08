e94ce6e6fd447f02b9d76ddd6bf5c999cdf1c9fd
SShGitHubUser <126820296+SShGitHubUser@users.noreply.github.com>
2023-03-08 13:49:12 +0200

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,3 @@
-OWM_TOKEN = '692f04b9abc71bb676ab2ad0e8583fb0'
-TGRAM_TOKEN = '5951472826:AAFbfKoAvSqJ8jWyi41NwkzUUR7eAabniSg'
 OWM_URL = ''.join(['https://api.openweathermap.org/data/2.5/weather?q={city}&appid=', OWM_TOKEN,'&units=metric'])
 TGRAM_URL = ''.join(['https://api.telegram.org/bot', TGRAM_TOKEN, '/{method}'])
 LAST_UPDATE_ID_FILE = 'last_update_id.txt'
